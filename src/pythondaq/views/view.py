1319a0fffdab49d3307bc34ee611dd0269434ae6
Thijs-de-Z <thijs.de.zeeuw8@gmail.com>
2022-11-17 10:42:03 +0100

    addition of docstrings removal of comments.

--- src/pythondaq/views/view.py
+++ src/pythondaq/views/view.py
@@ -1,18 +1,23 @@
 # primitive view to use for an experiment with an LED and resistor
-
 from pythondaq.models.diode_experiment import DiodeExperiment, init
 import matplotlib.pyplot as plt
 from datetime import date
 import os
 import pandas as pd
 
-# check if path exists and if writing is allowed
-def path_check(path):
 
-    # making new folder for current date if not done already
+def path_check(path):
+    """Checks if the path exists and is writable
+
+    Args:
+        path (string): Path to check the existanse and writablity of the path
+
+    Returns:
+        boolean: True/False dependent if the path is writable
+    """    
+
     if not os.path.exists(path):
 
-        # fixes error if path is non writeable
         try:
             os.makedirs(path)
 
@@ -23,53 +28,54 @@ def path_check(path):
     return True
 
 
-# saving of data in csv file:
 def data_to_csv(voltage_w_err, current_w_err):
+    """Saves the data into a csv file. This csv file will automaticly create a folder, data, in the parent file if this does not exist already.
+    The data is saved within seperate folders with the date when the experiment was conducted and it will also prevent overwriting previous saved data.
 
-    # new path with current date
+    Args:
+        voltage_w_err (list): 2 lists with the voltage of the experiment and it's error
+        current_w_err (list): 2 lists with the current of the experiment and it's error
+    """    
+
     today = f'{date.today()}'
     path = f'../data/{today}'
 
     if path_check(path):
         indx = 0
 
-        # new index for new measurement
         for filename in os.listdir(path):
             if filename.endswith(f'measurement_{indx}.csv'):
                 indx += 1
 
-        # saving of data
         df = {'voltage': voltage_w_err[0], 'error': voltage_w_err[1], 'current': current_w_err[0], 'error': current_w_err[1]}
         df = pd.DataFrame(df)
         df.to_csv(f'{path}/measurement_{indx}.csv', index = False, sep = ',')
 
 
 def i_u_characteristic():
+    """Calculates the I,U characteristics of a diode by running and experiment N times.
+    It will automatically show a visual presentation of the I,U characteristics with errors(if N>1).
+    Has the option to save the data to a csv file.
+    Setting of device and number of experiments is done through a text interface.
+    """    
 
-    # requesting of device to use
     print('devices')
     init()
     inpt = input("Please choose the index of the device to use: ")
 
-    # requesting amount of measurements
     N = input("How many measurements do you want to take? ")
 
-    # calling of class
     measurements = DiodeExperiment(inpt)
 
-    # starting of measurements with given values
     measurements.measurements(N = int(N), start = 0, stop = 1024)
 
-    # getting of values
     current = measurements.get_current()
     voltage = measurements.get_voltage()
     c_err = measurements.get_err_current()
     v_err = measurements.get_err_voltage()
 
-    # saving of data
-    if input("would you like to save the data [Y/N]? ") == 'Y' or 'y':
+    if input("would you like to save the data [Y/N]? ") == 'Y' or 'y' or 'yes':
         data_to_csv([voltage, v_err], [current, c_err])
 
-    # plotting of measured data
     plt.errorbar(voltage, current, yerr = c_err, xerr = v_err, markersize = 1, color = 'r', fmt = 'o' )
-    plt.show()
+    plt.show()          # still have to make the graph visuals better
